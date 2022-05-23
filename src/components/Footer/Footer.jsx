72f77c1e3387918f8d81fac2b7af5433e7047955
796242 <sahilgupta8445@gmail.com>
2022-05-23 18:34:23 +0530

    Initial commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="d-flex justify-content-center align-items-center text-white bg-black">
+    <footer className="d-flex justify-content-center align-items-center text-white bg-black mt-5">
       Copyright &copy; <span className="color-brand mx-2">PSPNEWS</span> All
       rightest reserved.
     </footer>
